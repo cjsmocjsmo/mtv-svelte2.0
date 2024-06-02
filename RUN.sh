9a9a07a606328b21ae383d0a877c6a7d4368682c
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-06-02 11:43:06 -0700

    feat: Add Dockerfile and scripts for building and running the application
    This commit adds a Dockerfile and scripts for building and running the application. The Dockerfile sets up an nginx server and copies the necessary files to serve the application. The scripts handle the build process and running the Docker container.

---
+++ RUN.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Check if there are at least two arguments provided
+if [ "$#" -ne 1 ]; then
+    echo "You must enter exactly 1 argument version (e.g., 0.0.1)";
+    exit 1;
+fi
+
+# # Check if the first argument is either 32 or 64
+# # If not, print an error message and exit the script
+# if [ "$1" != "32" ] && [ "$1" != "64" ]; then
+#     echo "The first argument must be either 32 or 64"
+#     exit 1
+# fi
+
+# Check if the second argument is a valid version string
+# If not, print an error message and exit the script
+if ! echo "$1" | grep -qE '^[0-9]+\.[0-9]+\.[0-9]+$'; then
+    echo "The second argument must be a valid version string (e.g., 0.0.1)";
+    exit 1;
+fi
+
+# If all checks pass, print the arguments
+echo "Version: $1";
+count=$(echo "$1" | sed 's/\.//g' )
+echo "mtvsvelte:$1";
+echo "mtvsvelte$count";
+
+# Build the Docker image
+
+git pull https://github.com/cjsmocjsmo/mtv-svelte2.0.git;
+
+npm install;
+
+npm run build;
+
+docker build -t mtvsvelte:$1 .;
+
+docker run --name mtvsvelte:$count -d -p 9090:9090 mtvsvelte:$1;
