0d8642689bc9929a4398dbfa8f11685c85d49e49
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-06-02 13:06:04 -0700

    chore: Update run.sh to correctly decrement count variable

--- run.sh
+++ run.sh
@@ -31,7 +31,7 @@ echo "mtvsvelte:$1";
 echo "mtvsvelte$count";
 echo "mtvsvelte$minusone";
 
-if [ $minusone == 0 ]; then
+if [ $minusone == "0" ]; then
     git pull https://github.com/cjsmocjsmo/mtv-svelte2.0.git;
 
     npm install;
@@ -43,7 +43,7 @@ if [ $minusone == 0 ]; then
     docker run --name mtvsvelte1 -d -p 8090:8090 mtvsvelte:$1;
 fi
 
-if [ $minusone == 1 ]; then
+if [ $minusone == "1" ]; then
     # Build the Docker image
 
     docker stop mtvsvelte1;
@@ -62,7 +62,7 @@ if [ $minusone == 1 ]; then
 fi
 
 
-if [ $minusone > 1 ]; then
+if [ $minusone > "1" ]; then
     # Build the Docker image
 
     docker stop mtvsvelte$minusone;
